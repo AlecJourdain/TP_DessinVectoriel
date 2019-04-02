f0a31e1bf4dc0ac376535657e423f5f806300467
Alec Jourdain <alecjourdain@hotmail.com>
2019-04-02 15:14:59 -0400

    creationMain

--- TP_DessinVectoriel/src/ca/csf/dfc/tp/Main.java
+++ TP_DessinVectoriel/src/ca/csf/dfc/tp/Main.java
@@ -1,17 +1,7 @@
-/**
- * 
- */
 package ca.csf.dfc.tp;
 
-/**
- * @author ManueLMaldonado
- *
- */
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
