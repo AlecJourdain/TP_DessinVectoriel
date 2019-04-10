196eabd63e86d1e888a3087e98537c47140f6b76
Manuel Alejandro Maldo <manuelalejo.maldo@gmail.com>
2019-04-09 23:16:11 -0400

    Espace de Travail

--- TP_DessinVectoriel/src/ca/csf/dfc/dessin/IModele.java
+++ TP_DessinVectoriel/src/ca/csf/dfc/dessin/IModele.java
@@ -13,13 +13,15 @@ void EspaceTravailDefault();
 	//pour l'espace Travail
 	
 	void setColorFondEspaceTravailDefault();
+	Color getColorFondEspaceTravailDefault();
 	
 	Color getColorFondEspaceTravail();
 	void setColorFondEspaceTravail(Color p_colorFondEspaceTravail) ;
 	
 	void setDimensionEspaceTravailDefault();	
 	void setDimensionEspaceTravail(double p_Largeur, double p_Hauteur);	
+	void setLargeurEspacetravail(int p_Largeur);
 	
-	double getLargeur();
-	double getHauteur();
+	double getLargeurEspaceTravail();
+	double getHauteurEspaceTravail();
 }
