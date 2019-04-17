648ecc6ef1df5ada226632b88d7deffb4957e0cb
keaven <keavencaron@gmail.com>
2019-04-17 09:36:56 -0400

    push changement mineur

--- TP_DessinVectoriel/src/ca/csf/dfc/fonctions/Sauvegarde.java
+++ TP_DessinVectoriel/src/ca/csf/dfc/fonctions/Sauvegarde.java
@@ -16,40 +16,57 @@ import ca.csf.dfc.vueUtilisateur.Canevas;
 
 public class Sauvegarde {
 	
+	private static final String ELEM_FORME="forme";
+	private static final String ELEM_DIMENSIONS="dimensions";
+	private static final String ELEM_TABLEAU_FORMES="tableau_formes";
+	/* attributs du canevas*/
+	private static final String ATTR_HAUTEUR_CANEVAS = "hauteur";
+	private static final String ATTR_LARGEUR_CANEVAS = "largeur";
+	/*attributs d'une forme*/
+	private static final String ATTR_TYPEFORME = "type";
+	private static final String ATTR_EPAISSEUR_TRAIT = "epaisseur_trait";
+	private static final String ATTR_COULEUR_CONTOUR = "couleur_trait";
+	private static final String ATTR_COULEUR_REMPLISSAGE = "couleur_remplissage";
+	private static final String ATTR_X1 = "x1";
+	private static final String ATTR_X2 = "x2";
+	private static final String ATTR_Y1 = "y1";
+	private static final String ATTR_Y2 = "y2";
+	
 	public static void ecrireFormesXML(XMLStreamWriter p_doc, ArrayList<Forme> p_dessin) throws XMLStreamException{
+		
 		for (Forme p_forme : p_dessin) {
 			// <forme ...
-			p_doc.writeStartElement("forme");
+			p_doc.writeStartElement(ELEM_FORME);
 			
 			//... type="..." ...
-			p_doc.writeAttribute("type", 
+			p_doc.writeAttribute(ATTR_TYPEFORME, 
 					p_forme.getType().toString());
 			
 			//... epaisseur_trait="..." ...
-			p_doc.writeAttribute("epaisseur_trait", 
+			p_doc.writeAttribute(ATTR_EPAISSEUR_TRAIT, 
 					Float.toString(p_forme.getEpaisseurTrait()));
 			
 			//... couleur_trait="..." ...
-			p_doc.writeAttribute("couleur_trait", Integer.toString(p_forme.getCouleurTrait().getRGB()));
+			p_doc.writeAttribute(ATTR_COULEUR_CONTOUR, Integer.toString(p_forme.getCouleurTrait().getRGB()));
 					
 			
 			//... couleur_remplissage="..." ...
-			p_doc.writeAttribute("couleur_remplissage",Integer.toString(p_forme.getCouleurRemplissage().getRGB()));
+			p_doc.writeAttribute(ATTR_COULEUR_REMPLISSAGE,Integer.toString(p_forme.getCouleurRemplissage().getRGB()));
 					
 			//... x1="..." ...
-			p_doc.writeAttribute("x1", 
+			p_doc.writeAttribute(ATTR_X1, 
 					Integer.toString(p_forme.getX1()));
 			
 			//... x2="..." ...
-			p_doc.writeAttribute("x2", 
+			p_doc.writeAttribute(ATTR_X2, 
 					Integer.toString(p_forme.getX2()));
 			
 			//... y1="..." ...
-			p_doc.writeAttribute("y1", 
+			p_doc.writeAttribute(ATTR_Y1, 
 					Integer.toString(p_forme.getY1()));
 			
 			//... y2="..." ...
-			p_doc.writeAttribute("y2", 
+			p_doc.writeAttribute(ATTR_Y2, 
 					Integer.toString(p_forme.getY2()));
 			
 			
@@ -75,13 +92,13 @@ public class Sauvegarde {
 					
 			
 			// <tableau_formes>
-			doc.writeStartElement("tableau_formes");
+			doc.writeStartElement(ELEM_TABLEAU_FORMES);
 			int hauteur=p_canevas.getDimensionEspaceTravail().height;
 			int largeur=p_canevas.getDimensionEspaceTravail().width;
 			
-			doc.writeStartElement("dimensions");
-			doc.writeAttribute("hauteur",String.valueOf(hauteur));
-			doc.writeAttribute("largeur",String.valueOf(largeur));
+			doc.writeStartElement(ELEM_DIMENSIONS);
+			doc.writeAttribute(ATTR_HAUTEUR_CANEVAS,String.valueOf(hauteur));
+			doc.writeAttribute(ATTR_LARGEUR_CANEVAS,String.valueOf(largeur));
 			doc.writeEndElement();
 			ecrireFormesXML(doc, p_canevas.getFormes());
 
